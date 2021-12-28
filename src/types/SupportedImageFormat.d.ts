13bcb4f9717d9adc1abf1f6a59f7ad173d1419a9
Lawrence Wagerfield <lawrence@projectone.io>
2021-12-28 14:11:01 +0000

    Remove problematic output image formats

--- src/types/SupportedImageFormat.d.ts
+++ src/types/SupportedImageFormat.d.ts
@@ -2,16 +2,16 @@
  * Supported output image format.
  */
 export type SupportedImageFormat =
-  | "heic"
-  | "heif"
   | "avif"
+  // | "gif" -- requires a custom build of lipvips (i.e. not the binary bundled with sharp).
+  // | "heic" -- produces un-open-able images & also times-out with large images
+  // | "heif" -- produces un-open-able images & also times-out with large images
+  // | "j2c" -- requires a custom build of lipvips (i.e. not the binary bundled with sharp).
+  // | "j2k" -- requires a custom build of lipvips (i.e. not the binary bundled with sharp).
+  // | "jp2" -- requires a custom build of lipvips (i.e. not the binary bundled with sharp).
+  // | "jpx" -- requires a custom build of lipvips (i.e. not the binary bundled with sharp).
   | "jpg"
   | "png"
-  | "raw"
+  // | "raw" -- unsupported output format
   | "tif"
-  | "webp"
-  | "gif"
-  | "jp2"
-  | "jpx"
-  | "j2k"
-  | "j2c";
+  | "webp";
